b1fd34f979fdaa0f3e78a24e70275ae762ee18dd
EvansMuthuri <105411576+EvansMuthuri@users.noreply.github.com>
2023-12-28 13:38:47 +0300

    Update settings.py

--- fail/settings.py
+++ fail/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s&!b(@h9()kx6l%r#n19=f^_-1uytgz&1t+!(5+(23+luws1xw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
